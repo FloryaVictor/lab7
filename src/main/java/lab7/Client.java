63971b9b735d9d235de0ff91b83b4399d9798573
FloryaVictor <lapunixa@yandex.ru>
2020-12-13 23:49:22 +0300

    Scripted auto-commit on change (2020-12-13 23:49:22) by gitwatch.sh

--- src/main/java/lab7/Client.java
+++ src/main/java/lab7/Client.java
@@ -1,9 +1,11 @@
 package lab7;
 
+import org.zeromq.ZContext;
+
 public class Client {
-    public static String server = "tcp://localhost "
+    public static String server = "tcp://localhost:8086";
 
     public static void main(String[] argv){
-
+        ZContext context = new ZContext();
     }
 }
