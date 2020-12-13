6b7f8b95474f1ed7eec40b58861fe35eefdf0044
FloryaVictor <lapunixa@yandex.ru>
2020-12-14 01:07:59 +0300

    Scripted auto-commit on change (2020-12-14 01:07:59) by gitwatch.sh

--- src/main/java/lab7/Server.java
+++ src/main/java/lab7/Server.java
@@ -22,7 +22,7 @@ public class Server {
         poller.register(storageSocket, ZMQ.Poller.POLLIN);
         while (!Thread.currentThread().isInterrupted()){
             if (poller.pollin(0)){
-                ZFrame frame = clientSocket.rec
+                ZFrame frame = clientSocket.unwa
             }
         }
         context.destroySocket(clientSocket);
