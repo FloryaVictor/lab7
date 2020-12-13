6a9d2aa2c35585ab8bf28a702b694787006f661e
FloryaVictor <lapunixa@yandex.ru>
2020-12-14 00:50:40 +0300

    Scripted auto-commit on change (2020-12-14 00:50:40) by gitwatch.sh

--- src/main/java/lab7/Server.java
+++ src/main/java/lab7/Server.java
@@ -1,7 +1,20 @@
 package lab7;
 
-public class Server {
-    public static void main(String[] argv){
+import org.zeromq.SocketType;
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
 
+public class Server {
+    private static final String clientSever = "tcp://localhost:8086";
+    private static final String storageSever = "tcp://localhost:8088";
+    private static final int CLIENT_SOCKET_NUMBER = 0;
+    private static final int STORAGE_SOCKET_NUMBER = 1;
+
+    public static void main(String[] argv){
+        ZContext context = new ZContext(1);
+        ZMQ.Socket clientSocket = context.createSocket(SocketType.ROUTER);
+        ZMQ.Socket storageSocket = context.createSocket(SocketType.ROUTER);
+        clientSocket.bind(clientSever);
+        storageSocket.bind(storageSever);
     }
 }
