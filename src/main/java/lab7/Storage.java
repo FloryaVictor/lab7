ebfb2450fb73a65359ca87c4b94aca149d5547e5
FloryaVictor <lapunixa@yandex.ru>
2020-12-13 23:30:22 +0300

    Scripted auto-commit on change (2020-12-13 23:30:22) by gitwatch.sh

--- src/main/java/lab7/Storage.java
+++ src/main/java/lab7/Storage.java
@@ -2,9 +2,14 @@ package lab7;
 
 import org.zeromq.ZMQ;
 
+import java.util.ArrayList;
+
 public class Storage {
+    private static ArrayList<String> cache;
     public static void main(String[] argv){
-        
-        for(String )
+        String start = argv[0];
+        for(int i = 1; i < argv.length; i++){
+            cache
+        }
     }
 }
