c66e44e9d400771a4e767eb3c380ff2daeecd998
FloryaVictor <lapunixa@yandex.ru>
2020-12-13 23:34:59 +0300

    Scripted auto-commit on change (2020-12-13 23:34:59) by gitwatch.sh

--- src/main/java/lab7/Storage.java
+++ src/main/java/lab7/Storage.java
@@ -9,6 +9,7 @@ public class Storage {
     public static void main(String[] argv){
         int start = Integer.parseInt(argv[0]);
         ArrayList<String> cache = new ArrayList<>(Arrays.asList(argv).subList(1, argv.length));
-        int end = start + cache.size() + 1;
+        int end = start + cache.size() - 1;
+        
     }
 }
