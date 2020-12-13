047f982f0124bc58eee27ee2741157a43a0971f3
FloryaVictor <lapunixa@yandex.ru>
2020-12-14 00:01:21 +0300

    Scripted auto-commit on change (2020-12-14 00:01:21) by gitwatch.sh

--- src/main/java/lab7/Storage.java
+++ src/main/java/lab7/Storage.java
@@ -1,15 +1,19 @@
 package lab7;
 
+import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Storage {
+    private static String server = "tcp://localhost:8088";
+
     public static void main(String[] argv){
         int start = Integer.parseInt(argv[0]);
         ArrayList<String> cache = new ArrayList<>(Arrays.asList(argv).subList(1, argv.length));
         int end = start + cache.size() - 1;
-
+        ZContext context = new ZContext(1);
+        ZMQ.Socket 
     }
 }
