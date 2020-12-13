158aaa507c912b6f2cdf1f90526cc0771047e871
FloryaVictor <lapunixa@yandex.ru>
2020-12-14 00:41:39 +0300

    Scripted auto-commit on change (2020-12-14 00:41:39) by gitwatch.sh

--- src/main/java/lab7/Storage.java
+++ src/main/java/lab7/Storage.java
@@ -10,6 +10,9 @@ import java.util.Arrays;
 public class Storage {
     private static final String server = "tcp://localhost:8088";
     private static final long NOTIFY_PERIOD = 5000;
+    private static final String NOTIFY = "notify";
+    private static final String GET = "get";
+    private static final String PUT = "put";
 
     public static void main(String[] argv){
         int start = Integer.parseInt(argv[0]);
@@ -21,10 +24,20 @@ public class Storage {
         long time = System.currentTimeMillis();
         while (!Thread.currentThread().isInterrupted()){
             if (System.currentTimeMillis() - time >= NOTIFY_PERIOD){
-                dealer.send(String.format("NOTIFY %d %d", start, end));
+                dealer.send(String.format("%s %d %d", NOTIFY, start, end));
                 time = System.currentTimeMillis();
             }
-            int index = Integer.parseInt(dealer.recvStr());
+            String msg = dealer.recvStr().toLowerCase();
+            if (msg.contains(GET)) {
+                int index = Integer.parseInt(msg.split(" ")[1]);
+                dealer.send(cache.get(index - start));
+            }
+            if (msg.contains(PUT)){
+                String[] split = msg.split(" ");
+                int index = Integer.parseInt(split[1]);
+                String value = split[2];
+                cache.set(index - start, value);
+            }
         }
         context.destroySocket(dealer);
         context.destroy();
