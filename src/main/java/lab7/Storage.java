ba493b47e943708b289eb2c11aedf5aefe85d0a2
FloryaVictor <lapunixa@yandex.ru>
2020-12-14 01:33:02 +0300

    Scripted auto-commit on change (2020-12-14 01:33:02) by gitwatch.sh

--- src/main/java/lab7/Storage.java
+++ src/main/java/lab7/Storage.java
@@ -4,12 +4,13 @@ import org.zeromq.SocketType;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
 
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Storage {
     private static final String server = "tcp://localhost:8088";
-    private static final long NOTIFY_PERIOD = 5000;
+    private static final long NOTIFY_PERIOD = 1000;
     private static final String NOTIFY = "notify";
     private static final String RESULT = "cache";
     private static final String GET = "get";
@@ -30,14 +31,19 @@ public class Storage {
             }
             String msg = dealer.recvStr().toLowerCase();
             if (msg.contains(GET)) {
-                int index = Integer.parseInt(msg.split(" ")[1]);
-                dealer.send(String.format("%s %s",RESULT, cache.get(index - start)));
+                try {
+                    int index = Integer.parseInt(msg.split(" ")[1]);
+                    dealer.send(String.format("%s %s", RESULT, cache.get(index - start)));
+                } catch (NumberFormatException ||IndexOutOfBoundsException ){
+                    dealer.send("error");
+                }
             }
             if (msg.contains(PUT)){
-                String[] split = msg.split(" ");
-                int index = Integer.parseInt(split[1]);
-                String value = split[2];
-                cache.set(index - start, value);
+                try {
+                    String[] split = msg.split(" ");
+                    int index = Integer.parseInt(split[1]);
+                    String value = split[2];
+                    cache.set(index - start, value);
             }
         }
         context.destroySocket(dealer);
