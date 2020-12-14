e2b162eeeba0eef13240acbcbf432d2252c229f0
FloryaVictor <lapunixa@yandex.ru>
2020-12-14 11:07:08 +0300

    Scripted auto-commit on change (2020-12-14 11:07:08) by gitwatch.sh

--- src/main/java/lab7/Client.java
+++ src/main/java/lab7/Client.java
@@ -20,7 +20,7 @@ public class Client {
             }
             ZFrame frame = new ZFrame(command);
             frame.send(client, 0);
-            ZMsg resp = ZFrame.recvFrame()
+            ZMsg resp = ZFrame.recvFrame(client);
             System.out.println(client.recvStr(0));
         }
         context.destroySocket(client);
