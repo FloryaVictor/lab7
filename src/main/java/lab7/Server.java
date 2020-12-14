e4deff971e4acfad1f09522c984a8cc851a4fff3
FloryaVictor <lapunixa@yandex.ru>
2020-12-14 11:08:46 +0300

    Scripted auto-commit on change (2020-12-14 11:08:46) by gitwatch.sh

--- src/main/java/lab7/Server.java
+++ src/main/java/lab7/Server.java
@@ -25,6 +25,7 @@ public class Server {
             if (poller.pollin(0)){
                 ZMsg zmsg = ZMsg.recvMsg(clientSocket);
                 String msg = zmsg.getLast().toString().toLowerCase();
+                System.out.println(msg);
                 if (msg.contains("get")){
                     try {
                         String[] split = msg.split(" ");
@@ -70,6 +71,7 @@ public class Server {
                 ZMsg zmsg = ZMsg.recvMsg(clientSocket);
                 zmsg.unwrap();
                 String msg = zmsg.getLast().toString().toLowerCase();
+                System.out.println(msg);
                 if (msg.contains("notify")){
                     try {
                         String[] split = msg.split(" ");
