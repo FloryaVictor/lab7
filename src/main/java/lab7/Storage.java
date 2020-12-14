70c72b6c72fdb3e828c4cff95f054f4ca4b65341
FloryaVictor <lapunixa@yandex.ru>
2020-12-14 12:02:02 +0300

    Scripted auto-commit on change (2020-12-14 12:02:02) by gitwatch.sh

--- src/main/java/lab7/Storage.java
+++ src/main/java/lab7/Storage.java
@@ -39,6 +39,7 @@ public class Storage {
                 ZMsg zmsg = ZMsg.recvMsg(dealer);
                 String msg = zmsg.getLast().toString().toLowerCase();
                 if (msg.contains(GET)) {
+                    System.out.println("get");
                     try {
                         int index = Integer.parseInt(msg.split(" ")[1]);
                         zmsg.getLast().reset(String.format("%s %s", RESULT, cache.get(index - start)));
